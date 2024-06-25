73fb04e8f9913b4d670567fb49e7f6e009f1649a
JWbase <jungwoo1002@gmail.com>
2024-06-25 18:22:51 +0900

    학습 테스트

---
+++ src/test/java/tobyspring/hellospring/learningtest/ClockTest.java
@@ -0,0 +1,40 @@
+package tobyspring.hellospring.learningtest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.junit.jupiter.api.Test;
+
+public class ClockTest {
+
+    // Clock를 이용해서 LocalDateTime.now?
+    @Test
+    void clock() {
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        assertThat(dt2).isAfter(dt1);
+    }
+
+
+    // Clock를 Test에서 사용할 때 내가 원하는 시간을 지정해서 현재 시간을 가져오게 할 수 있는가?
+    @Test
+    void fixedClock() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        LocalDateTime dt3 = LocalDateTime.now(clock).plusMinutes(30);
+
+        assertThat(dt2).isEqualTo(dt1);
+        assertThat(dt3).isEqualTo(dt1.plusMinutes(30));
+    }
+
+}
