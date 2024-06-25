b59877530a5966536d9259db9f278d6ceb948d6f
JWbase <jungwoo1002@gmail.com>
2024-06-25 16:48:52 +0900

    test와 DI(2)

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,40 @@
+package tobyspring.hellospring.payment;
+
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.TestObjectFactory;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
+class PaymentServiceSpringTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    ExRateProviderStub exRateProviderStub;
+
+    @Test
+    void convertedAmount() throws IOException {
+        // exRate: 1000
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+        // exRate:500
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+
+    }
+}
