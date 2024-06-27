34679f8019fdd11ecf98448b92b3c0a2e7a8b1a8
JWbase <jungwoo1002@gmail.com>
2024-06-27 15:22:53 +0900

    @Transcational과 AOP

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.hellospring.data.JdbcOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
 import tobyspring.hellospring.order.OrderServiceImpl;
-import tobyspring.hellospring.order.OrderServiceTxProxy;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
 
     @Bean
@@ -25,8 +26,6 @@ public class OrderConfig {
         PlatformTransactionManager transactionManager,
         OrderRepository orderRepository
     ) {
-        return new OrderServiceTxProxy(
-            new OrderServiceImpl(orderRepository),
-            transactionManager);
+        return new OrderServiceImpl(orderRepository);
     }
 }
